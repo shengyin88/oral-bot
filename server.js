f103d4c9d2d14b57bd23c9373ab488fa7848c469
shengyin88 <yingsheng@vitaminm.sg>
2026-03-31 23:37:46 +0800

    Update server.js

--- server.js
+++ server.js
@@ -25,6 +25,10 @@ app.post('/webhook', (req, res) => {
   res.sendStatus(200);
 });
 
-app.listen(3000, () => {
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
   console.log("Server running on port 3000");
 });

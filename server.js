48da06e21b8d0962ec8d8f65ad187473cfc94f9c
shengyin88 <yingsheng@vitaminm.sg>
2026-03-31 23:26:31 +0800

    Add files via upload

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+const VERIFY_TOKEN = "oral_bot_123";
+
+app.get('/webhook', (req, res) => {
+  const mode = req.query['hub.mode'];
+  const token = req.query['hub.verify_token'];
+  const challenge = req.query['hub.challenge'];
+
+  if (mode === 'subscribe' && token === VERIFY_TOKEN) {
+    console.log("Webhook verified!");
+    res.status(200).send(challenge);
+  } else {
+    res.sendStatus(403);
+  }
+});
+
+app.post('/webhook', (req, res) => {
+  console.log("Incoming message:");
+  console.log(JSON.stringify(req.body, null, 2));
+
+  res.sendStatus(200);
+});
+
+app.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
